4dfae1e7e4d5596bc5c9205966d7319b374ca074
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-02-15 09:03:39 +0700

    navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,54 @@
+<header class="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+    <nav
+        class="mt-4 relative max-w-2xl w-full bg-[#fff]/60 backdrop-blur-md border border-gray-200 rounded-[24px] mx-4 flex flex-wrap md:flex-nowrap items-center justify-between p-1 ps-5 md:ps-6 md:py-0 sm:mx-auto">
+        <div class="flex items-center">
+            <!-- Logo -->
+            <a class="flex text-xl items-center justify-start gap-2 md:gap-3 focus:outline-hidden focus:opacity-80"
+                href="index.html" aria-label="Preline">
+                <img src="/images/logo.webp" class="w-6 md:w-8" alt="">
+                <p class="font-fugaz mt-1">Patunes</p>
+            </a>
+        </div>
+
+        <div class="flex items-center gap-1 md:order-4 md:ms-4">
+            <div class="md:hidden">
+                <!-- Toggle Button -->
+                <button type="button"
+                    class="hs-collapse-toggle flex justify-center items-center size-10 bg-white border border-gray-200 text-gray-500 rounded-full hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
+                    id="hs-pro-pnm-collapse" aria-expanded="false" aria-controls="hs-pro-pnm"
+                    aria-label="Toggle navigation" data-hs-collapse="#hs-pro-pnm">
+                    <svg class="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
+                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <line x1="3" x2="21" y1="6" y2="6" />
+                        <line x1="3" x2="21" y1="12" y2="12" />
+                        <line x1="3" x2="21" y1="18" y2="18" />
+                    </svg>
+                    <svg class="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
+                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M18 6 6 18" />
+                        <path d="m6 6 12 12" />
+                    </svg>
+                </button>
+                <!-- End Toggle Button -->
+            </div>
+        </div>
+
+        <div id="hs-pro-pnm"
+            class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
+            aria-labelledby="hs-pro-pnm-collapse" role="region">
+            <div
+                class="flex flex-col md:flex-row items-center md:justify-end gap-4 md:gap-5 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
+                <a class="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 hover:text-black focus:outline-hidden focus:gray-100 border-transparent text-gray-800/60 transition duration-300"
+                    href="" aria-current="page">Beranda</a>
+                <a class="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 hover:text-black focus:outline-hidden focus:gray-100 border-transparent text-gray-800/60 transition duration-300"
+                    href="" aria-current="page">Terbaru</a>
+                <a class="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 hover:text-black focus:outline-hidden focus:gray-100 border-transparent text-gray-800/60 transition duration-300"
+                    href="" aria-current="page">Katalog</a>
+                    <a class="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 hover:text-black focus:outline-hidden focus:gray-100 border-transparent text-gray-800/60 transition duration-300"
+                    href="" aria-current="page">Kontak</a>
+            </div>
+        </div>
+    </nav>
+</header>
