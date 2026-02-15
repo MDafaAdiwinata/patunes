c7e01be4e539b604c1b455493f0943792910493a
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-02-15 16:25:41 +0700

    FAQ, CTA, Footer Component - Slicing UI Landing Page

---
+++ resources/views/components/cta.blade.php
@@ -0,0 +1,19 @@
+<section class="py-24">
+    <div class="container mx-auto px-6">
+        <div class="bg-[#ECEFF1] border border-black/10 rounded-3xl flex flex-col text-center items-center justify-center p-8 lg:p-16">
+            <h1 class="text-[#2a2a2a] font-bold text-xl md:text-2xl lg:text-3xl xl:text-5xl">
+                Siap Tampil Keren dengan Brand Lokal?
+            </h1>
+            <h1
+                class="text-[#2a2a2a]/80 w-full md:w-2/3 lg:w-1/2 font-normal text-md md:text-lg lg:text-xl xl:text-2xl text-center my-2 md:my-6">
+                Jelajahi katalog kami sekarang dan temukan sepatu impian yang pas buat gaya
+                kamu. Lengkap, dan GRATIS!
+            </h1>
+
+            <a href="/register"
+                class="bg-[#558DBA] text-white mt-8 px-5 py-2.5 text-md md:text-lg text-center font-bold rounded-xl md:rounded-2xl transition duration-300 hover:scale-105">
+                Coba Sekarang!
+            </a>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/footer-landing.blade.php
@@ -0,0 +1,27 @@
+<footer class="bg-[#2A2A2A]">
+    <div class="mx-auto w-full container px-12 py-10">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="/images/logo-light.webp" class="h-8" alt="Patunes Logo" />
+                <span class="self-center font-fugaz text-2xl text-white mt-1">Patunes</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm md:text-base font-light text-[#f0f0f0]/80 sm:mb-0">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Beranda</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Terbaru</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Katalog</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Kontak</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-[#f0f0f0]/20 sm:mx-auto lg:my-8" />
+        <span class="block text-sm md:text-base text-[#f0f0f0]/60 sm:text-center dark:text-gray-400">© 2026 Patunes. By <a
+                href="https://github.com/mdafaadiwinata" target="_blank" class="underline font-semibold">Dafa Adi</a> dalam Kegiatan Bi Got Talent 2026.</span>
+    </div>
+</footer>
